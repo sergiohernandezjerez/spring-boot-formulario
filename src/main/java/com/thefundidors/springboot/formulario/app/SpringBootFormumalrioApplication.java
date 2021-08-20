1530eae00863983ac4f9b67d56ce551af4f915bc
sergiohernandezjerez <sergiohernandezjerez@gmail.com>
2021-08-20 08:49:07 +0200

    clone of

---
+++ src/main/java/com/thefundidors/springboot/formulario/app/SpringBootFormumalrioApplication.java
@@ -0,0 +1,13 @@
+package com.thefundidors.springboot.formulario.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormumalrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormumalrioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thefundidors/springboot/formulario/app/SpringBootFormumalrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefundidors.springboot.formulario.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormumalrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
